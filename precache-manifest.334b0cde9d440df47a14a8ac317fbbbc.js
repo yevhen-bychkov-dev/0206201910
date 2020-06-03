11a6b0dc12343877f3899ea18d91c2eb5626f59d
WincherZ <wincher2016@gmail.com>
2020-06-03 14:03:18 +0300

    Updates

---
+++ precache-manifest.334b0cde9d440df47a14a8ac317fbbbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51486325956a6c0cb6624122a1e099fc",
+    "url": "/0206201910/index.html"
+  },
+  {
+    "revision": "83e07a225faa3ee0620e",
+    "url": "/0206201910/static/css/main.6e93c121.chunk.css"
+  },
+  {
+    "revision": "8cbddb4a5b24ac0a2ae0",
+    "url": "/0206201910/static/js/2.f5a16fbf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/0206201910/static/js/2.f5a16fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e07a225faa3ee0620e",
+    "url": "/0206201910/static/js/main.5d85535b.chunk.js"
+  },
+  {
+    "revision": "259f815e66cab1d84cc5",
+    "url": "/0206201910/static/js/runtime-main.e5dee2f6.js"
+  }
+]);
